b10c21df4302601dd844ff3156f9ff5c5787db44
David Kershner <david.kershner@unisys.com>
2015-09-23 14:16:46 -0400

    staging: unisys: Add support to update Features bits in channel queues
    Add support to visorbus to update the features in the channel queues.
    Signal queues features is the memory location to disable/enable signal
    queue interrupts.
    
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>

--- include/linux/visorbus.h
+++ include/linux/visorbus.h
@@ -337,6 +337,10 @@ int visorchannel_signalinsert(struct visorchannel *channel, u32 queue,
 			      void *msg);
 bool visorchannel_signalempty(struct visorchannel *channel, u32 queue);
 const guid_t *visorchannel_get_guid(struct visorchannel *channel);
+int visorchannel_set_sig_features(struct visorchannel *channel, u32 queue,
+				  u64 features);
+int visorchannel_clear_sig_features(struct visorchannel *channel, u32 queue,
+				    u64 features);
 
 #define BUS_ROOT_DEVICE UINT_MAX
 struct visor_device *visorbus_get_device_by_id(u32 bus_no, u32 dev_no,
