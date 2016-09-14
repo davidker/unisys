davidker/unisys_attic

staging: unisys: visorbus: Move postcode enums to global visorbus interface
visorbus will be providing the hypervisor postcode interface so move the
required postcode definitions there.

Signed-off-by: Bryan Thompson <bryan.thompson@unisys.com>

drivers/staging/unisys/include/visorbus.h
@@ -38,6 +38,7 @@
 #include <linux/slab.h>
 
 #include "channel.h"
+#include "diagchannel.h"
 
 struct visor_driver;
 struct visor_device;
@@ -278,6 +279,47 @@ void visorbus_enable_channel_interrupts(struct visor_device *dev);
  * @dev: the device on which to disable interrupts
  */
 void visorbus_disable_channel_interrupts(struct visor_device *dev);
+
+enum driver_pc {		/* POSTCODE driver identifier tuples */
+	/* visorbus driver files */
+	VISOR_BUS_PC = 0xB0,
+	VISOR_BUS_PC_visorbus_main_c = 0xB1,
+	VISOR_BUS_PC_visorchipset_c = 0xB2,
+};
+
+enum event_pc {			/* POSTCODE event identifier tuples */
+	BUS_CREATE_ENTRY_PC = 0x001,
+	BUS_CREATE_FAILURE_PC = 0x002,
+	BUS_CREATE_EXIT_PC = 0x003,
+	BUS_CONFIGURE_ENTRY_PC = 0x004,
+	BUS_CONFIGURE_FAILURE_PC = 0x005,
+	BUS_CONFIGURE_EXIT_PC = 0x006,
+	CHIPSET_INIT_ENTRY_PC = 0x007,
+	CHIPSET_INIT_SUCCESS_PC = 0x008,
+	CHIPSET_INIT_FAILURE_PC = 0x009,
+	CHIPSET_INIT_EXIT_PC = 0x00A,
+	CONTROLVM_INIT_FAILURE_PC = 0x00B,
+	DEVICE_CREATE_ENTRY_PC = 0x00C,
+	DEVICE_CREATE_FAILURE_PC = 0x00D,
+	DEVICE_CREATE_SUCCESS_PC = 0x00E,
+	DEVICE_CREATE_EXIT_PC = 0x00F,
+	DEVICE_ADD_PC = 0x010,
+	DEVICE_REGISTER_FAILURE_PC = 0x011,
+	DEVICE_CHANGESTATE_FAILURE_PC = 0x012,
+	DRIVER_ENTRY_PC = 0x013,
+	DRIVER_EXIT_PC = 0x014,
+	MALLOC_FAILURE_PC = 0x015,
+	CRASH_DEV_ENTRY_PC = 0x016,
+	CRASH_DEV_EXIT_PC = 0x017,
+	CRASH_DEV_RD_BUS_FAILURE_PC = 0x018,
+	CRASH_DEV_RD_DEV_FAILURE_PC = 0x019,
+	CRASH_DEV_BUS_NULL_FAILURE_PC = 0x01A,
+	CRASH_DEV_DEV_NULL_FAILURE_PC = 0x01B,
+	CRASH_DEV_CTRL_RD_FAILURE_PC = 0x01C,
+	CRASH_DEV_COUNT_FAILURE_PC = 0x01D,
+	SAVE_MSG_BUS_FAILURE_PC = 0x01E,
+	SAVE_MSG_DEV_FAILURE_PC = 0x01F,
+};
 
 /**
  * visorchannel_signalremove() - removes a message from the designated

davidker/unisys

Signed-off-by: David Kershner <david.kershner@unisys.com>

drivers/staging/unisys/include/visorbus.h
@@ -206,6 +206,47 @@ enum diag_severity {
 	DIAG_SEVERITY_PRINT = 4,
 };
 
+enum driver_pc {		/* POSTCODE driver identifier tuples */
+	/* visorbus driver files */
+	VISOR_BUS_PC = 0xF0,
+	VISOR_BUS_PC_visorbus_main_c = 0xFF,
+	VISOR_BUS_PC_visorchipset_c = 0xFE,
+};
+
+enum event_pc {			/* POSTCODE event identifier tuples */
+	BUS_CREATE_ENTRY_PC = 0x001,
+	BUS_CREATE_FAILURE_PC = 0x002,
+	BUS_CREATE_EXIT_PC = 0x003,
+	BUS_CONFIGURE_ENTRY_PC = 0x004,
+	BUS_CONFIGURE_FAILURE_PC = 0x005,
+	BUS_CONFIGURE_EXIT_PC = 0x006,
+	CHIPSET_INIT_ENTRY_PC = 0x007,
+	CHIPSET_INIT_SUCCESS_PC = 0x008,
+	CHIPSET_INIT_FAILURE_PC = 0x009,
+	CHIPSET_INIT_EXIT_PC = 0x00A,
+	CONTROLVM_INIT_FAILURE_PC = 0x00B,
+	DEVICE_CREATE_ENTRY_PC = 0x00C,
+	DEVICE_CREATE_FAILURE_PC = 0x00D,
+	DEVICE_CREATE_SUCCESS_PC = 0x00E,
+	DEVICE_CREATE_EXIT_PC = 0x00F,
+	DEVICE_ADD_PC = 0x010,
+	DEVICE_REGISTER_FAILURE_PC = 0x011,
+	DEVICE_CHANGESTATE_FAILURE_PC = 0x012,
+	DRIVER_ENTRY_PC = 0x013,
+	DRIVER_EXIT_PC = 0x014,
+	MALLOC_FAILURE_PC = 0x015,
+	CRASH_DEV_ENTRY_PC = 0x016,
+	CRASH_DEV_EXIT_PC = 0x017,
+	CRASH_DEV_RD_BUS_FAILURE_PC = 0x018,
+	CRASH_DEV_RD_DEV_FAILURE_PC = 0x019,
+	CRASH_DEV_BUS_NULL_FAILURE_PC = 0x01A,
+	CRASH_DEV_DEV_NULL_FAILURE_PC = 0x01B,
+	CRASH_DEV_CTRL_RD_FAILURE_PC = 0x01C,
+	CRASH_DEV_COUNT_FAILURE_PC = 0x01D,
+	SAVE_MSG_BUS_FAILURE_PC = 0x01E,
+	SAVE_MSG_DEV_FAILURE_PC = 0x01F,
+};
+
 bool visorchannel_signalremove(struct visorchannel *channel, u32 queue,
 			       void *msg);
 bool visorchannel_signalinsert(struct visorchannel *channel, u32 queue,