f403b01848a57987e189e7ffffa3b5adbda06874
Jon Frisch <jon.frisch@unisys.com>
2016-10-07 17:57:44 -0400

    staging: unisys: add visor driver version to channel area
    Allows the visor driver version to be visible from the channel area.
    
    Signed-off-by: Jon Frisch <jon.frisch@unisys.com>
    Signed-off-by: David Kershner <david.kershner@unisys.com>

--- drivers/staging/unisys/visorhba/visorhba_main.c
+++ drivers/staging/unisys/visorhba/visorhba_main.c
@@ -1134,6 +1134,7 @@ static void visorhba_remove(struct visor_device *dev)
  */
 static struct visor_driver visorhba_driver = {
 	.name = "visorhba",
+	.version = VERSION,
 	.owner = THIS_MODULE,
 	.channel_types = visorhba_channel_types,
 	.probe = visorhba_probe,
--- drivers/staging/unisys/visornic/visornic_main.c
+++ drivers/staging/unisys/visornic/visornic_main.c
@@ -1957,6 +1957,7 @@ static int visornic_resume(struct visor_device *dev,
  */
 static struct visor_driver visornic_driver = {
 	.name = "visornic",
+	.version = VERSION,
 	.owner = THIS_MODULE,
 	.channel_types = visornic_channel_types,
 	.probe = visornic_probe,
