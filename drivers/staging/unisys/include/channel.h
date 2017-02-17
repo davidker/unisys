4be1c066ac72d1190abcb4da6899e4aef308f44e
David Kershner <david.kershner@unisys.com>
2017-02-16 23:39:35 -0500

    staging: unisys: remove client from spar_check_channel
    We only have one version of spar_check_channel, get rid of the client
    at the end and help save space.

--- drivers/staging/unisys/include/channel.h
+++ drivers/staging/unisys/include/channel.h
@@ -204,12 +204,12 @@ struct signal_queue_header {
  * is used to pass the EFI_DIAG_CAPTURE_PROTOCOL needed to log messages.
  */
 static inline int
-spar_check_channel_client(void __iomem *ch,
-			  uuid_le expected_uuid,
-			  char *chname,
-			  u64 expected_min_bytes,
-			  u32 expected_version,
-			  u64 expected_signature)
+spar_check_channel(void __iomem *ch,
+		   uuid_le expected_uuid,
+		   char *chname,
+		   u64 expected_min_bytes,
+		   u32 expected_version,
+		   u64 expected_signature)
 {
 	if (uuid_le_cmp(expected_uuid, NULL_UUID_LE) != 0) {
 		uuid_le guid;
