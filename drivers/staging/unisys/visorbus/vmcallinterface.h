beda51eb9af1499ec689a83a64ca408f5196bff8
Sameer Wadgaonkar <sameer.wadgaonkar@unisys.com>
2017-05-04 17:55:46 -0400

    staging: unisys: visorbus: vmcallinterface.h: fixed comments formating issues
    Removed comments from the right side of the lines.
    
    githubissue: #131
    Signed-off-by: Sameer Wadgaonkar <sameer.wadgaonkar@unisys.com>

--- drivers/staging/unisys/visorbus/vmcallinterface.h
+++ drivers/staging/unisys/visorbus/vmcallinterface.h
@@ -14,19 +14,19 @@
 
 #ifndef __VMCALLINTERFACE_H__
 #define __VMCALLINTERFACE_H__
-
-enum vmcall_monitor_interface_method_tuple { /* VMCALL identification tuples  */
-	    /* Note: when a new VMCALL is added:
-	     * - the 1st 2 hex digits correspond to one of the
-	     *   VMCALL_MONITOR_INTERFACE types and
-	     * - the next 2 hex digits are the nth relative instance of within a
-	     *   type
-	     * E.G. for VMCALL_VIRTPART_RECYCLE_PART,
-	     * - the 0x02 identifies it as a VMCALL_VIRTPART type and
-	     * - the 0x01 identifies it as the 1st instance of a VMCALL_VIRTPART
-	     *   type of VMCALL
-	     */
-	/* used by all Guests, not just IO */
+/* VMCALL identification tuples  */
+enum vmcall_monitor_interface_method_tuple {
+	/* Note: when a new VMCALL is added:
+	 * - the 1st 2 hex digits correspond to one of the
+	 *   VMCALL_MONITOR_INTERFACE types and
+	 * - the next 2 hex digits are the nth relative instance of within a
+	 *   type
+	 * E.G. for VMCALL_VIRTPART_RECYCLE_PART,
+	 * - the 0x02 identifies it as a VMCALL_VIRTPART type and
+	 * - the 0x01 identifies it as the 1st instance of a VMCALL_VIRTPART
+	 *   type of VMCALL
+	 */
+	/* Used by all Guests, not just IO */
 	VMCALL_CONTROLVM_ADDR = 0x0501,
 };
 
@@ -44,11 +44,14 @@ enum vmcall_result {
 struct vmcall_io_controlvm_addr_params {
 	/* The Guest-relative physical address of the ControlVm channel. */
 	/* This VMCall fills this in with the appropriate address. */
-	u64 address;	/* contents provided by this VMCALL (OUT) */
-	/* the size of the ControlVm channel in bytes This VMCall fills this */
-	/* in with the appropriate address. */
-	u32 channel_bytes;	/* contents provided by this VMCALL (OUT) */
-	u8 unused[4];		/* Unused Bytes in the 64-Bit Aligned Struct */
+	/* Contents provided by this VMCALL (OUT) */
+	u64 address;
+	/* The size of the ControlVm channel in bytes This VMCall fills this */
+	/* In with the appropriate address. */
+	/* Contents provided by this VMCALL (OUT) */
+	u32 channel_bytes;
+	/* Unused Bytes in the 64-Bit Aligned Struct */
+	u8 unused[4];
 } __packed;
-
-#endif /* __VMCALLINTERFACE_H__ */
+/* __VMCALLINTERFACE_H__ */
+#endif
