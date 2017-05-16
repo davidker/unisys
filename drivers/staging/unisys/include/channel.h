bde00d67500e59270e127aaecf9bebd10a1dd40a
Sameer Wadgaonkar <sameer.wadgaonkar@unisys.com>
2017-05-16 16:11:03 -0400

    staging: unisys: include: channel.h: fixed comments formating issues
    Removed comments from the right side of the lines.
    
    githubissue: #131
    Signed-off-by: Sameer Wadgaonkar <sameer.wadgaonkar@unisys.com>

--- drivers/staging/unisys/include/channel.h
+++ drivers/staging/unisys/include/channel.h
@@ -35,28 +35,26 @@
 #define VISOR_CHANNEL_SIGNATURE SIGNATURE_32('E', 'C', 'N', 'L')
 
 enum channel_serverstate {
-	CHANNELSRV_UNINITIALIZED = 0,	/* channel is in an undefined state */
-	CHANNELSRV_READY = 1	/* channel has been initialized by server */
+	/* Channel is in an undefined state. */
+	CHANNELSRV_UNINITIALIZED = 0,
+	/* Channel has been initialized by server. */
+	CHANNELSRV_READY = 1
 };
 
 enum channel_clientstate {
 	CHANNELCLI_DETACHED = 0,
-	CHANNELCLI_DISABLED = 1,	/* client can see channel but is NOT
-					 * allowed to use it unless given TBD
-					 * explicit request (should actually be
-					 * < DETACHED)
-					 */
-	CHANNELCLI_ATTACHING = 2,	/* legacy EFI client request
-					 * for EFI server to attach
-					 */
-	CHANNELCLI_ATTACHED = 3,	/* idle, but client may want
-					 * to use channel any time
-					 */
-	CHANNELCLI_BUSY = 4,	/* client either wants to use or is
-				 * using channel
-				 */
-	CHANNELCLI_OWNED = 5	/* "no worries" state - client can */
-				/* access channel anytime */
+	/* Client can see channel but is NOT allowed to use it unless given TBD
+	 * explicit request (should actually be < DETACHED).
+	 */
+	CHANNELCLI_DISABLED = 1,
+	/* Legacy EFI client request for EFI server to attach. */
+	CHANNELCLI_ATTACHING = 2,
+	/* Idle, but client may want to use channel any time. */
+	CHANNELCLI_ATTACHED = 3,
+	/* Client either wants to use or is using channel. */
+	CHANNELCLI_BUSY = 4,
+	/* "No worries" state - client can access channel anytime. */
+	CHANNELCLI_OWNED = 5
 };
 
 #define VISOR_CHANNEL_SERVER_READY(ch) \
@@ -81,17 +79,17 @@ enum channel_clientstate {
 	 ? (1) : (0))
 
 /* Values for VISORA_CHANNEL_PROTOCOL.CliErrorBoot: */
-/* throttling invalid boot channel statetransition error due to client
+/* Throttling invalid boot channel statetransition error due to client
  * disabled
  */
 #define VISOR_CLIERRORBOOT_THROTTLEMSG_DISABLED 0x01
 
-/* throttling invalid boot channel statetransition error due to client
+/* Throttling invalid boot channel statetransition error due to client
  * not attached
  */
 #define VISOR_CLIERRORBOOT_THROTTLEMSG_NOTATTACHED 0x02
 
-/* throttling invalid boot channel statetransition error due to busy channel */
+/* Throttling invalid boot channel statetransition error due to busy channel */
 #define VISOR_CLIERRORBOOT_THROTTLEMSG_BUSY 0x04
 
 /* Values for VISOR_CHANNEL_PROTOCOL.Features: This define exists so
@@ -110,48 +108,61 @@ enum channel_clientstate {
 
 /* Common Channel Header */
 struct channel_header {
-	u64 signature;		/* Signature */
-	u32 legacy_state;	/* DEPRECATED - being replaced by */
-			/* SrvState, CliStateBoot, and CliStateOS below */
-	u32 header_size;	/* sizeof(struct channel_header) */
-	u64 size;		/* Total size of this channel in bytes */
-	u64 features;		/* Flags to modify behavior */
-	uuid_le chtype;		/* Channel type: data, bus, control, etc. */
-	u64 partition_handle;	/* ID of guest partition */
-	u64 handle;		/* Device number of this channel in client */
-	u64 ch_space_offset;	/* Offset in bytes to channel specific area */
-	u32 version_id;		/* struct channel_header Version ID */
-	u32 partition_index;	/* Index of guest partition */
-	uuid_le zone_uuid;	/* Guid of Channel's zone */
-	u32 cli_str_offset;	/* offset from channel header to
-				 * nul-terminated ClientString (0 if
-				 * ClientString not present)
-				 */
-	u32 cli_state_boot;	/* CHANNEL_CLIENTSTATE of pre-boot
-				 * EFI client of this channel
-				 */
-	u32 cmd_state_cli;	/* CHANNEL_COMMANDSTATE (overloaded in
-				 * Windows drivers, see ServerStateUp,
-				 * ServerStateDown, etc)
-				 */
-	u32 cli_state_os;	/* CHANNEL_CLIENTSTATE of Guest OS
-				 * client of this channel
-				 */
-	u32 ch_characteristic;	/* CHANNEL_CHARACTERISTIC_<xxx> */
-	u32 cmd_state_srv;	/* CHANNEL_COMMANDSTATE (overloaded in
-				 * Windows drivers, see ServerStateUp,
-				 * ServerStateDown, etc)
-				 */
-	u32 srv_state;		/* CHANNEL_SERVERSTATE */
-	u8 cli_error_boot;	/* bits to indicate err states for
-				 * boot clients, so err messages can
-				 * be throttled
-				 */
-	u8 cli_error_os;	/* bits to indicate err states for OS
-				 * clients, so err messages can be
-				 * throttled
-				 */
-	u8 filler[1];		/* Pad out to 128 byte cacheline */
+	/* Signature */
+	u64 signature;
+	/* DEPRECATED - being replaced by */
+	u32 legacy_state;
+	/* SrvState, CliStateBoot, and CliStateOS below */
+	/* sizeof(struct channel_header) */
+	u32 header_size;
+	/* Total size of this channel in bytes */
+	u64 size;
+	/* Flags to modify behavior */
+	u64 features;
+	/* Channel type: data, bus, control, etc. */
+	uuid_le chtype;
+	/* ID of guest partition */
+	u64 partition_handle;
+	/* Device number of this channel in client */
+	u64 handle;
+	/* Offset in bytes to channel specific area */
+	u64 ch_space_offset;
+	/* Struct channel_header Version ID */
+	u32 version_id;
+	/* Index of guest partition */
+	u32 partition_index;
+	/* Guid of Channel's zone */
+	uuid_le zone_uuid;
+	/* Offset from channel header to null-terminated ClientString
+	 * (0 if * ClientString not present)
+	 */
+	u32 cli_str_offset;
+	/* CHANNEL_CLIENTSTATE of pre-boot EFI client of this channel */
+	u32 cli_state_boot;
+	/* CHANNEL_COMMANDSTATE (overloaded in Windows drivers,
+	 * see ServerStateUp, ServerStateDown, etc)
+	 */
+	u32 cmd_state_cli;
+	/* CHANNEL_CLIENTSTATE of Guest OS client of this channel */
+	u32 cli_state_os;
+	/* CHANNEL_CHARACTERISTIC_<xxx> */
+	u32 ch_characteristic;
+	/* CHANNEL_COMMANDSTATE (overloaded in Windows drivers,
+	 * see ServerStateUp,* ServerStateDown, etc)
+	 */
+	u32 cmd_state_srv;
+	/* CHANNEL_SERVERSTATE */
+	u32 srv_state;
+	/* Bits to indicate err states for boot clients, so err messages can be
+	 * throttled.
+	 */
+	u8 cli_error_boot;
+	/* Bits to indicate err states for OS clients, so err messages can be
+	 * throttled.
+	 */
+	u8 cli_error_os;
+	/* Pad out to 128 byte cacheline */
+	u8 filler[1];
 	/* Please add all new single-byte values below here */
 	u8 recover_channel;
 } __packed;
@@ -161,41 +172,52 @@ struct channel_header {
 /* Subheader for the Signal Type variation of the Common Channel */
 struct signal_queue_header {
 	/* 1st cache line */
-	u32 version;		/* SIGNAL_QUEUE_HEADER Version ID */
-	u32 chtype;		/* Queue type: storage, network */
-	u64 size;		/* Total size of this queue in bytes */
-	u64 sig_base_offset;	/* Offset to signal queue area */
-	u64 features;		/* Flags to modify behavior */
-	u64 num_sent;		/* Total # of signals placed in this queue */
-	u64 num_overflows;	/* Total # of inserts failed due to
-				 * full queue
-				 */
-	u32 signal_size;	/* Total size of a signal for this queue */
-	u32 max_slots;		/* Max # of slots in queue, 1 slot is
-				 * always empty
-				 */
-	u32 max_signals;	/* Max # of signals in queue
-				 * (MaxSignalSlots-1)
-				 */
-	u32 head;		/* Queue head signal # */
+	/* SIGNAL_QUEUE_HEADER Version ID */
+	u32 version;
+	/* Queue type: storage, network */
+	u32 chtype;
+	/* Total size of this queue in bytes */
+	u64 size;
+	/* Offset to signal queue area */
+	u64 sig_base_offset;
+	/* Flags to modify behavior */
+	u64 features;
+	/* Total # of signals placed in this queue */
+	u64 num_sent;
+	/* Total # of inserts failed due to full queue */
+	u64 num_overflows;
+	/* Total size of a signal for this queue */
+	u32 signal_size;
+	/* Max # of slots in queue, 1 slot is always empty */
+	u32 max_slots;
+	/* Max # of signals in queue (MaxSignalSlots-1) */
+	u32 max_signals;
+	/* Queue head signal # */
+	u32 head;
 	/* 2nd cache line */
-	u64 num_received;	/* Total # of signals removed from this queue */
-	u32 tail;		/* Queue tail signal */
-	u32 reserved1;		/* Reserved field */
-	u64 reserved2;		/* Reserved field */
+	/* Total # of signals removed from this queue */
+	u64 num_received;
+	/* Queue tail signal */
+	u32 tail;
+	/* Reserved field */
+	u32 reserved1;
+	/* Reserved field */
+	u64 reserved2;
 	u64 client_queue;
-	u64 num_irq_received;	/* Total # of Interrupts received.  This
-				 * is incremented by the ISR in the
-				 * guest windows driver
-				 */
-	u64 num_empty;		/* Number of times that visor_signal_remove
-				 * is called and returned Empty Status.
-				 */
-	u32 errorflags;		/* Error bits set during SignalReinit
-				 * to denote trouble with client's
-				 * fields
-				 */
-	u8 filler[12];		/* Pad out to 64 byte cacheline */
+	/* Total # of Interrupts received. This	is incremented by the ISR in the
+	 * guest windows driver.
+	 */
+	u64 num_irq_received;
+	/* Number of times that visor_signal_remove is called and returned
+	 * Empty Status.
+	 */
+	u64 num_empty;
+	/* Error bits set during SignalReinit to denote trouble with client's
+	 * fields.
+	 */
+	u32 errorflags;
+	/* Pad out to 64 byte cacheline. */
+	u8 filler[12];
 } __packed;
 
 /* Generic function useful for validating any type of channel when it is
@@ -220,7 +242,8 @@ visor_check_channel(struct channel_header *ch,
 			return 0;
 		}
 	}
-	if (expected_min_bytes > 0) {	/* verify channel size */
+	/* verify channel size */
+	if (expected_min_bytes > 0) {
 		if (ch->size < expected_min_bytes) {
 			pr_err("Channel mismatch on channel=%s(%pUL) field=size expected=0x%-8.8Lx actual=0x%-8.8Lx\n",
 			       chname, &expected_uuid,
@@ -229,7 +252,8 @@ visor_check_channel(struct channel_header *ch,
 			return 0;
 		}
 	}
-	if (expected_version > 0) {	/* verify channel version */
+	/* verify channel version */
+	if (expected_version > 0) {
 		if (ch->version_id != expected_version) {
 			pr_err("Channel mismatch on channel=%s(%pUL) field=version expected=0x%-8.8lx actual=0x%-8.8x\n",
 			       chname, &expected_uuid,
@@ -238,7 +262,8 @@ visor_check_channel(struct channel_header *ch,
 			return 0;
 		}
 	}
-	if (expected_signature > 0) {	/* verify channel signature */
+	/* verify channel signature */
+	if (expected_signature > 0) {
 		if (ch->signature != expected_signature) {
 			pr_err("Channel mismatch on channel=%s(%pUL) field=signature expected=0x%-8.8Lx actual=0x%-8.8Lx\n",
 			       chname, &expected_uuid,
