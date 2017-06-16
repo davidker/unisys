5895e14287c9c89b8920515cb49aa0b41965ad92
Steve Wilson <steven.d.wilson@unisys.com>
2017-06-16 12:54:02 -0400

    staging: unisys: visorbus: adjust tabs in function
    Removed extra tabs in visorbus_get_device_by_id funtion.
    
    Signed-off-by: Steve Wilson <steven.d.wilson@unisys.com>

--- drivers/staging/unisys/visorbus/visorchipset.c
+++ drivers/staging/unisys/visorbus/visorchipset.c
@@ -364,8 +364,8 @@ struct visor_device *visorbus_get_device_by_id(u32 bus_no, u32 dev_no,
 	struct device *dev_start = NULL;
 	struct visor_device *vdev = NULL;
 	struct visor_busdev id = {
-			.bus_no = bus_no,
-			.dev_no = dev_no
+		.bus_no = bus_no,
+		.dev_no = dev_no
 		};
 
 	if (from)
