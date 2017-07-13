849bcd117711a655e85e922161c80b8001ba7b3a
David Kershner <david.kershner@unisys.com>
2017-07-13 16:12:00 -0400

    staging: unisys: visorbus: Consolidate controlvm channel creation.
    The functions to create the controlvm channel were disjointed and ignoring
    information that was available. This patch consolidates it so it clearer
    what is happening.
    
    Signed-off-by: David Kershner <david.kershner@unisys.com>

--- drivers/staging/unisys/visorbus/visorchipset.c
+++ drivers/staging/unisys/visorbus/visorchipset.c
@@ -1332,32 +1332,27 @@ error:
 	}
 }
 
-static unsigned int issue_vmcall_io_controlvm_addr(u64 *control_addr,
-						   u32 *control_bytes)
+static int controlvm_channel_create(void)
 {
-	u64 physaddr;
+	u64 addr;
+	u32 size;
 	int err;
 
-	physaddr = virt_to_phys(&chipset_dev->controlvm_params);
-	err = unisys_vmcall(VMCALL_CONTROLVM_ADDR, physaddr);
+	err = unisys_vmcall(VMCALL_CONTROLVM_ADDR,
+			    virt_to_phys(&chipset_dev->controlvm_params));
 	if (err)
 		return err;
 
-	*control_addr = chipset_dev->controlvm_params.address;
-	*control_bytes = chipset_dev->controlvm_params.channel_bytes;
+	addr = chipset_dev->controlvm_params.address;
+	size = chipset_dev->controlvm_params.channel_bytes;
+
+	chipset_dev->controlvm_channel = visorchannel_create_with_lock(
+						  addr, size, GFP_KERNEL,
+						  &visor_controlvm_channel_guid);
+	if (!chipset_dev->controlvm_channel)
+		return -ENOMEM;
 
 	return 0;
-}
-
-static u64 controlvm_get_channel_address(void)
-{
-	u64 addr = 0;
-	u32 size = 0;
-
-	if (issue_vmcall_io_controlvm_addr(&addr, &size))
-		return 0;
-
-	return addr;
 }
 
 static void setup_crash_devices_work_queue(struct work_struct *work)
@@ -1746,25 +1741,20 @@ static int visorchipset_init(struct acpi_device *acpi_device)
 	if (!chipset_dev)
 		goto error;
 
-	addr = controlvm_get_channel_address();
-	if (!addr)
-		goto error;
+	err = controlvm_channel_create();
+	if (err)
+		goto error_free_chipset_dev;
 
 	acpi_device->driver_data = chipset_dev;
 	chipset_dev->acpi_device = acpi_device;
 	chipset_dev->poll_jiffies = POLLJIFFIES_CONTROLVMCHANNEL_FAST;
-	controlvm_channel = visorchannel_create_with_lock(addr, 0, GFP_KERNEL,
-						&visor_controlvm_channel_guid);
-	if (!controlvm_channel)
-		goto error_free_chipset_dev;
-
-	chipset_dev->controlvm_channel = controlvm_channel;
 
 	err = sysfs_create_groups(&chipset_dev->acpi_device->dev.kobj,
 				  visorchipset_dev_groups);
 	if (err < 0)
 		goto error_destroy_channel;
 
+	controlvm_channel = chipset_dev->controlvm_channel;
 	if (!visor_check_channel(visorchannel_get_header(controlvm_channel),
 				 &visor_controlvm_channel_guid,
 				 "controlvm",
