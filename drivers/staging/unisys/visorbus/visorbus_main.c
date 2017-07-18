ff3b1474f04928edb1ee2ea5f2e304c635e4bc74
Sameer Wadgaonkar <sameer.wadgaonkar@unisys.com>
2017-07-18 12:18:39 -0400

    staging: unisys: visorbus: Get rid of passthrough function visorchipset_bus_destroy
    The function visorchipset_bus_destroy called viosrbus_remove_instance,
    we can just combile it with visorbus_remove_instance.
    
    Signed-off-by: Sameer Wadgaonkar <sameer.wadgaonkar@unisys.com>

--- drivers/staging/unisys/visorbus/visorbus_main.c
+++ drivers/staging/unisys/visorbus/visorbus_main.c
@@ -1086,7 +1086,7 @@ err_debugfs_dir:
  * visorbus_remove_instance() - remove a device instance for the visorbus itself
  * @dev: struct visor_device indentifying the bus to remove
  */
-static void visorbus_remove_instance(struct visor_device *dev)
+void visorbus_remove_instance(struct visor_device *dev)
 {
 	kfree(dev->vbus_hdr_info);
 	list_del(&dev->list_all);
@@ -1102,6 +1102,7 @@ static void visorbus_remove_instance(struct visor_device *dev)
 	 */
 	list_del(&dev->list_all);
 	device_unregister(&dev->device);
+	visorbus_response(dev, 0, CONTROLVM_BUS_DESTROY);
 }
 
 /*
@@ -1117,12 +1118,6 @@ static void remove_all_visor_devices(void)
 						      list_all);
 		remove_visor_device(dev);
 	}
-}
-
-void visorchipset_bus_destroy(struct visor_device *dev)
-{
-	visorbus_remove_instance(dev);
-	visorbus_response(dev, 0, CONTROLVM_BUS_DESTROY);
 }
 
 int visorchipset_device_create(struct visor_device *dev_info)
